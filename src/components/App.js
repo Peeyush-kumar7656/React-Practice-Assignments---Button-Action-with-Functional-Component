d7cd7e942d5ea4f1b53f7663d1accf6b531885a4
@Singh_Peeyush <43371031+Peeyush-kumar7656@users.noreply.github.com>
2021-05-19 13:17:47 +0530

    modify

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 function App() {
-  handleClick = () => {
-    <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-    }
+  const [renderP,setRenderP]=useState(0);
   return (
     <div id="main">
-      // Do not alter the main div
-      <button id = "click"  onClick={handleClick}>click</button>
+    {renderP ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:
+      <button id = "click"  onClick={() =>setRenderP(renderP=>renderP+1)} >click</button>}
     </div>
   );
 }
